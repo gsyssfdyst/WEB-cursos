81935cee6d49ec193bac74cb93486c5d0a018765
tablet-linux <gsyssfdyst@gmail.com>
2025-06-17 18:00:30 -0300

    Funçaoe, null

--- Exercicios-js/fundamentos/nullUndefined.js
+++ Exercicios-js/fundamentos/nullUndefined.js
@@ -2,5 +2,19 @@ let valor // nao inicializado
 console.log(valor); // Exibe undefined, pois valor não foi inicializado
 
 valor = null // Ausência de valor
+console.log(valor);
+//console.log(valor.topString())
 
+const produto = {};
+console.log(produto.preco);
+console.log(produto)
 
+produto.preco = 3.50;
+console.log(produto);
+
+produto.preco = undefined; // evite atribuir undefined 
+// detelar p.produto.preco
+console.log(produto);
+
+produto.preco = null; 
+console.log()
